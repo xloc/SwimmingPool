842504f534d6d2b472d894a27697dadbd42dc574
Cadmium-CD <512256738@qq.com>
2017-03-08 16:40:01 +0800

    Add files via upload
    This program allow us to process 36 pictures in to gradient ones, and it also allowed us to input the driving information for every pictures by hand. The two data(pictures and direction information) will be saved in the same file.npz for later machining learning.

--- tryings/trackDetect/Pattern Split/pattern.py
+++ tryings/trackDetect/Pattern Split/pattern.py
@@ -6,12 +6,19 @@ __author__ = 'xloc'
 
 import glob
 import cv2
-import random
+#import random
 import numpy as np
-#from matplotlib import pyplot as plt  
+  
+global image_array
+global label_array
+image_array = np.zeros((1, 30600))
+label_array = np.zeros((1, 4), 'float')
+k = np.zeros((4, 4), 'float')
+for i in range(4):
+    k[i, i] = 1
 
 
-def load_img(path, ratio=0.5, show=True):
+def load_img(path, ratio=0.25, show=True):
     im = cv2.imread(path)
 
     h,w = im.shape[0:2]
@@ -25,7 +32,10 @@ def load_img(path, ratio=0.5, show=True):
 
 
 def convertjpg(jpgfile):
-    im = load_img(jpgfile, ratio=0.5, show=False)
+    global image_array
+    global label_array
+    
+    im = load_img(jpgfile, ratio=0.25, show=False)
     v = im[:,:,2]
     dx, dy = np.gradient(v)
     g = (dx**2 + dy**2)**0.5
@@ -34,17 +44,38 @@ def convertjpg(jpgfile):
     g = cv2.erode(g, core)
     g = g.astype(dtype=np.uint8)
     g = cv2.equalizeHist(g)
-    #core = cv2.getStructuringElement(cv2.MORPH_RECT, (2,2))
-    #g = cv2.erode(g, core)
-#    plt.imshow(g, cmap=plt.cm.gray)
-#    row_g = g.shape[0]
-#    g = g[row_g/2,:]
-    i = str(random.uniform(10, 20)) 
-    cv2.imwrite('E:/TDPS/Dataf/p/'+i+'.jpg', g)
-
+    core = cv2.getStructuringElement(cv2.MORPH_RECT, (2,2))
+    g = cv2.erode(g, core)
+    g = g[150:320,:]
+    
+    
+    #存储路径
+    temp_array = g.reshape(1, 30600).astype(np.float32)
+    image_array = np.vstack((image_array, temp_array))
+    
+    
+    
+    
+#原图路径
 for jpgfile in glob.glob("E:/TDPS/Dataf/*.jpg"):
     convertjpg(jpgfile)
+
+
+
+for i in range(36):
+    print (" %d"%(i))
+    m = int(raw_input())
+    temp_lable = k[m]
+    label_array = np.vstack((label_array, temp_lable))
     
+    
+    
+    
+train = image_array[1:, :]   
+train_labels = label_array[1:, :]
+#包含测试数据的文件（需要重命名，可以自己加路径）
+np.savez('test08.npz', train=train, train_labels=train_labels)
+print train.shape
     
 
 
